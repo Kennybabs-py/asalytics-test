6b2b97f5e1adc93301182533ab103a490a37961b
Kennybabs-py <babalolakehinde74@gmail.com>
2022-08-11 00:11:03 +0100

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { Fragment } from "react";
+import Head from "next/head";
+import "styles/globals.scss";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Head>
+        <title>ASAlytics</title>
+        <meta name="description" content="Asalytics assessment" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>{" "}
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
